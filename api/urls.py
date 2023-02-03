797cac01dd1350c7735d729286373e9a475f833b
Sagar Singh <sagarsi0202@gmail.com>
2023-02-04 01:01:01 +0530

    add download api, get tag and get details api

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    # path('', home.as_view(), name='home'),
     re_path(r'api/get_tag', get_tag.as_view(), name='get_tag'),
+    re_path(r'api/download_video', download_video.as_view(), name='download_video'),
+    re_path(r'api/video_details', video_details.as_view(), name='video_details'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ import logging
 from pytube import YouTube
 
 
+# Get Youtube Tag
 class get_tag(APIView):
 
     def post(request, *args, **kwargs):
@@ -26,19 +27,92 @@ class get_tag(APIView):
                 youtube_tag = videotags(url)
                 if youtube_tag:
                     response = {"Success": f"{youtube_tag}"}
-                    logging.info(f"===========================================================================================================")
                     logging.info(f"URL :: >>> {url}")
                     logging.info(f"Tags :: >>> {response}")
                     return Response(response, status=status.HTTP_200_OK)
                 else:
                     response = {"Warning": f"No Tags Found"}
-                    logging.warning(f"===========================================================================================================")
                     logging.warning(f"URL :: >>> {url}")
                     logging.warning(f"Error :: >>> {response}")
                     return Response(response, status=status.HTTP_202_ACCEPTED)
         except Exception as e:
             response = {"Error": f"{e}"}
-            logging.error(f"===========================================================================================================")
+            logging.error(f"URL :: >>> {url}")
+            logging.error(f"Tags :: >>> {response}")
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+# Download Youtube Video
+class download_video(APIView):
+
+    def post(request, *args, **kwargs):
+        logging.basicConfig(
+            filename="logs/download_video.log",
+            filemode='a',
+            format='%(asctime)s :: >>> %(levelname)s %(message)s',
+            datefmt='%H:%M:%S',
+            level=logging.INFO
+        )
+
+        try:
+            body = request.request.data
+
+            if body.get('url'):
+                url = body['url']
+                youtubeObject = YouTube(url)
+                youtubeObject = youtubeObject.streams.get_highest_resolution()
+                path = "logs/"
+                try:
+                    youtubeObject.download(path)
+                    response = {"Success": f"Successfully Downloaded"}
+                    logging.info(f"URL :: >>> {url}")
+                    logging.info(f"Download :: >>> {response}")
+                    return Response(response, status=status.HTTP_200_OK)
+                except Exception as e:
+                    response = {"Warning": f"{e}"}
+                    logging.warning(f"URL :: >>> {url}")
+                    logging.warning(f"Error :: >>> {response}")
+                    return Response(response, status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            response = {"Error": f"{e}"}
+            logging.error(f"URL :: >>> {url}")
+            logging.error(f"Tags :: >>> {response}")
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+# View Youtube Video Details
+class video_details(APIView):
+
+    def post(request, *args, **kwargs):
+        logging.basicConfig(
+            filename="logs/video_details.log",
+            filemode='a',
+            format='%(asctime)s :: >>> %(levelname)s %(message)s',
+            datefmt='%H:%M:%S',
+            level=logging.INFO
+        )
+
+        try:
+            body = request.request.data
+
+            if body.get('url'):
+                url = body['url']
+                youtubeObject = YouTube(url)
+                # youtubeObject = youtubeObject.streams.get_highest_resolution()
+                try:
+                    title = youtubeObject.title
+                    desc = youtubeObject.description
+                    thumbnail = youtubeObject.thumbnail_url
+                    response = {"success": [{"title" : f"{title}", "desc" : f"{desc}", "thumbnail" : f"{thumbnail}" } ]}
+                    logging.info(f"URL :: >>> {url}")
+                    logging.info(f"Download :: >>> {response}")
+                    return Response(response, status=status.HTTP_200_OK)
+                except Exception as e:
+                    response = {"Warning": f"{e}"}
+                    logging.warning(f"URL :: >>> {url}")
+                    logging.warning(f"Error :: >>> {response}")
+                    return Response(response, status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            response = {"Error": f"{e}"}
             logging.error(f"URL :: >>> {url}")
             logging.error(f"Tags :: >>> {response}")
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
